45205629e850ad89e7f44958005df8682f5f91a7
Nathan Geffen <nathangeffen@gmail.com>
2017-06-08 22:55:08 +0200

    Refined experiments.

--- R/generalAnalysis.R
+++ R/generalAnalysis.R
@@ -1,3 +1,29 @@
+library("xtable")
+
+confinterval_02_5 <- function(vals) {
+  tryCatch(
+    {
+      confinterval(vals)[1]
+    },
+    error = function(e) {
+      NA
+    }
+  )
+}
+
+confinterval_97_5 <- function(vals) {
+  tryCatch(
+    {
+      confinterval(vals)[2]
+    },
+    error = function(e) {
+      NA
+    }
+  )
+}
+
+
+
 args = commandArgs(trailingOnly=TRUE)
 
 if (length(args)==0) {
@@ -28,7 +54,28 @@ meanTimings = aggregate(timings$Value, by=list(timings$Name),
                              FUN=mean)
 
 
-sprintf("%s,%f", meanPrevalences$Group.1, meanPrevalences$x)
-sprintf("%s,%f", meanPartnerships$Group.1, meanPartnerships$x)
-sprintf("%s,%f", meanTimings$Group.1, meanTimings$x)
+ci_prevalences_02_5 = aggregate(prevalences$Value,
+                                by=list(prevalences$Name,
+                                        prevalences$Desc2),
+                                FUN=confinterval_02_5)
+ci_prevalences_97_5 = aggregate(prevalences$Value,
+                                by=list(prevalences$Name,
+                                        prevalences$Desc2),
+                                FUN=confinterval_97_5)
 
+
+print("Mean prevalence")
+sprintf("%s,%f", meanPrevalences$Group.1, meanPrevalences$x)
+print("0.025 CI prevalence")
+sprintf("%s,%f", ci_prevalences_02_5$Group.1, ci_prevalences_02_5$x)
+print("0.0975 CI prevalence")
+sprintf("%s,%f", ci_prevalences_97_5$Group.1, ci_prevalences_97_5$x)
+print("Mean partnerships")
+sprintf("%s,%f", meanPartnerships$Group.1, meanPartnerships$x)
+print("Mean timings")
+sprintf("%s,%f", meanTimings$Group.1, meanTimings$x)
+xtable(meanPrevalences)
+xtable(ci_prevalences_02_5)
+xtable(ci_prevalences_97_5)
+xtable(meanPartnerships)
+xtable(meanTimings)
