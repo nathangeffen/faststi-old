d54c918510598d74d386fdc1ff0c7fd6ab1183fc
Eduard Grebe <eduardgrebe@sun.ac.za>
2017-06-19 16:58:11 +0200

    Busy writing notebook on fitting procedures

--- R/fitfaststi_easyABC.R
+++ R/fitfaststi_easyABC.R
@@ -18,12 +18,23 @@ initial_prevs <- function(msw = 0.01,
   return(as.list(environment()))
 }
 
+# Age categories
+
 infection_parms <- function(inf_msw = 0.1,
                             inf_msm = 0.2,
                             inf_wsm = 0.05,
                             inf_wsw = 0.05) {
   return(as.list(environment()))
 }
+
+
+# MIN_AGE_TRUNCATE
+# MAX_AGE_TRUNCATE -> can be left out then 12-100
+# Sex survey data problematic after 40 or 50
+# parameter to ask for analysis only between MIN and MAX
+# additional parameter -> follow truncated at start people to the end
+# INC_TRUNCATED_AGE -> increment truncated age as simulation progresses
+# analyzeTruncatedAge _Init _During _After (default = FALSE)
 
 write_input <- function(name = "RTEST_", 
                         n_simulations = 100, 
@@ -36,7 +47,7 @@ write_input <- function(name = "RTEST_",
                         parms = infection_parms()) {
   # Write initial rates to file
   with(prevs, {
-    initial_rates <- data.frame(AGE = c(14,19,24,29,34,39,44,49,100),
+    initial_rates <- data.frame(AGE = c(14,19,24,29,34,39,44,49,100), # 14 - everyone up to 14.99999, 19 = 15-19.9999
                                 MSW = c(rep(msw,8),0),
                                 MSM = c(rep(msw,8),0),
                                 WSM = c(rep(wsm,8),0),
@@ -52,13 +63,16 @@ write_input <- function(name = "RTEST_",
     lines <- paste0(lines, "NUM_SIMULATIONS ", n_simulations, "\n")
     lines <- paste0(lines, "NUM_AGENTS ", n_agents, "\n")
     lines <- paste0(lines, "NUM_THREADS ", n_threads, "\n")
-    lines <- paste0(lines, "START_DATE ", start_date, "\nEND_DATE ", end_date, "\nMATCH_NEIGHBORS 30\n")
-    lines <- paste0(lines, "MATCH_CLUSTERS 200\nMATCH_EVENT CSPM\n")
+    lines <- paste0(lines, "START_DATE ", start_date, "\nEND_DATE ", end_date, "\n")
+    lines <- paste0(lines, "MATCH_EVENT CSPM\n") # CSPM / RKPM / BLOSSOMV
+    lines <- paste0(lines, "MATCH_NEIGHBORS 10\n") # 
+    lines <- paste0(lines, "MATCH_CLUSTERS 5\n") #
     lines <- paste0(lines, "ANALYZE_DURING_SIM ", analysis_step_days, "\n")
     lines <- paste0(lines, "HET_MALE_INFECTIOUSNESS ", inf_msw, "\n")
     lines <- paste0(lines, "HOM_MALE_INFECTIOUSNESS ", inf_msm, "\n")
     lines <- paste0(lines, "HET_FEMALE_INFECTIOUSNESS ", inf_wsm, "\n")
     lines <- paste0(lines, "HOM_FEMALE_INFECTIOUSNESS ", inf_wsw, "\n")
+    lines <- paste0(lines, "BREAKUP_EVENT LIMIT\nMATING_POOL_EVENT LIMIT\n")
     lines <- paste0(lines, "INITIAL_INFECTION_RATES_CSV InitialRates.csv")
     write(lines, file="FaststiInput.csv")
   })
@@ -97,6 +111,10 @@ while(mean_final_prevalence < 0.3) {
   msw_transprob <- msw_transprob + step
 }
 
+# BREAKUP_EVENT -> FREQUENCY / LIMIT / RANDOM
+# MATING_POOL -> FREQUENCY / LIMIT / RANDOM
+# above must be the same
+
 set_priors <- function(msw.tprob.mean = 0.1,
                        sd = 0.1,
                        ratio.msmmsw = 2,
@@ -109,13 +127,14 @@ set_priors <- function(msw.tprob.mean = 0.1,
   return(priors)
 }
 
-faststi_simulation <- function(param.set) {
+faststi_simulation <- function(seed = 123, param.set) {
   # This is an ugly workaround for the fact that EasyABC only allows parameters
   # for which priors are set
   fixed <- get("fixed_params",env=globalenv())
   
   with(fixed, {
     write_input(n_simulations = n_simulations,
+                n_agents = 10000,
                 start_date = start_date,
                 end_date = end_date,
                 prevs = prevs,
@@ -124,47 +143,47 @@ faststi_simulation <- function(param.set) {
                                         inf_wsm = param.set[3],
                                         inf_wsw = param.set[4]))
   })
-  cmd = sprintf("./bin/faststi -f %s >%s", "FaststiInput.csv", "FaststiOutput.csv")
+  time <- toString(lubridate::now())
+  outfilename <- paste0("FaststiOutput_",gsub(" ","_",gsub(":","-",time)),".csv")
+  cmd = sprintf("./bin/faststi -s %i -f %s >%s", seed, "FaststiInput.csv", outfilename)
   system(cmd)
-  results <- read.csv("FaststiOutput.csv", TRUE)
+  results <- read.csv(outfilename, TRUE)
   final_prevalences <- dplyr::filter(results, Desc2 == "PREVALENCE", Date == max(Date))
   mean_final_prevalence = mean(final_prevalences$Value)
   sd_final_prevalence = sd(final_prevalences$Value)
-  return(c(mean_final_prevalence,sd_final_prevalence))
+  print(mean_final_prevalence)
+  return(c(mean_final_prevalence))
 }
+
+
 
 fixed_params <-  list(n_simulations = 4,
                       n_threads = 4,
-                      start_date = 2005,
+                      start_date = 2015,
                       end_date = 2020,
                       prevs = initial_prevs(msw = 0.01, 
                                             msm = 0.01, 
                                             wsm = 0.01, 
                                             wsw = 0))
-#priors <- list(prior.msw.tprob = c("unif",0.05,0.5),
-#               prior.msm.tprob = c("unif",0.2,0.6),
-#               prior.wsm.tprob = c("unif",0.01,0.2),
-#               prior.wsw.tprob = c("unif",0.01,0.2))
-priors <- set_priors()
+priors <- list(prior.msw.tprob = c("unif",0,1),
+               prior.msm.tprob = c("unif",0,1),
+               prior.wsm.tprob = c("unif",0,1),
+               prior.wsw.tprob = c("unif",0,1))
+
+faststi_simulation(param.set = c(0.05,0.05,0.05,0.05))
+
+
+#priors <- set_priors()
 library(EasyABC)
-sim.results <- ABC_rejection(model=faststi_simulation,
-                                        prior=priors,
-                                        prior_test = "X2 > X1 && X3 < X1 && X3 > X4",
-                                        summary_stat_target=c(0.3,0.1),
-                                        tol = 0.2,
-                                        progress_bar=TRUE,
-                                        nb_simul = 20)
-sim.df <- as.data.frame(sim.results)
-View(sim.df)
 sim.results <- ABC_sequential(model=faststi_simulation,
                               method = "Lenormand",
                              prior=priors,
                              prior_test = "X2 > X1 && X3 < X1 && X3 > X4",
-                             summary_stat_target = c(0.3,0.1),
-                             dist_weights = c(0.8,0.2)
+                             summary_stat_target = 0.05,
                              progress_bar=TRUE,
                              nb_simul = 20,
                              alpha = 0.5,
-                             p_acc_min = 0.1)
+                             p_acc_min = 0.05)
 sim.df <- as.data.frame(sim.results)
 View(sim.df)
+
