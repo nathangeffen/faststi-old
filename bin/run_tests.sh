e429e60123423655c1864a3dfe297ed694c1131e
Nathan Geffen <nathangeffen@gmail.com>
2017-07-25 10:13:27 +0200

    New casual sex file.

--- bin/run_tests.sh
+++ bin/run_tests.sh
@@ -6,10 +6,23 @@ then
 else
 	filename=$1
 fi
+
+if [ "$2" == "" ]
+then
+	csvfile=tmp.csv
+else
+	csvfile=$2
+fi
+
+
 echo `date` Compiling
 make release
 echo `date` Starting faststi
-./faststi -f $filename >tmp.csv
+./faststi -f $filename >$csvfile
 echo `date` Starting general analysis
-Rscript R/generalAnalysis.R tmp.csv
+Rscript R/generalAnalysis.R $csvfile
+echo #################
+echo # PARAMETER FILE: $filename
+echo ################
+cat $filename
 echo `date` All done
